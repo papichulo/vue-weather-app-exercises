23a4e135469b283b89a2232d71c9a040c6fe1d22
Marcus Alfredsson <marcus@alfredo.se>
2019-04-13 12:59:09 +0200

    Finished first exercise

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
   export default {
     name: 'app',
     data: () => ({
-      // component state goes here
+      cities: [],
+      cityString: '',
     }),
     methods: {
-      // method for updating input value goes here
-      // method for click event goes here
+      updateCityString(e) {
+        this.cityString = e.target.value;
+      },
+      addCity() {
+        this.cities.push(this.cityString);
+        this.cityString = '';
+      },
     }
   }
 </script>
@@ -14,8 +20,16 @@
 <template>
   <div id="app">
     <h2>Weather App</h2>
+    <div>
+      <input type="input" @change="updateCityString" v-model="cityString"/>
+      <button type="button" @click="addCity">add</button>
+    </div>
+    <div v-for="city in cities" :key="city">
+      <p>{{city}}</p>
+    </div>
   </div>
 </template>
+
 
 <style>
 #app {
